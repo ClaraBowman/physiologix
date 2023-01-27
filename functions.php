d3d253740622d0e94ec42cf41a09b3b25e7510f8
David Matthew <davidmatthew.ie@gmail.com>
2023-01-27 15:09:41 +0000

    Blog styling updates

--- functions.php
+++ functions.php
@@ -27,6 +27,7 @@ add_action( 'wp_enqueue_scripts', function() {
 add_action( 'after_setup_theme', function() {
     
     add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support( 'responsive-embeds' );
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
 
